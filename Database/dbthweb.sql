f6a0e8a21b65ee58a2a5b203bad57f48968bf527
TranBaoPhuc <phuc423vn@gmail.com>
2022-01-05 21:54:38 +0700

    [Database] Update database second time

--- Database/dbthweb.sql
+++ Database/dbthweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th1 02, 2022 lúc 03:05 PM
+-- Thời gian đã tạo: Th1 05, 2022 lúc 02:50 PM
 -- Phiên bản máy phục vụ: 5.7.31
 -- Phiên bản PHP: 7.4.9
 
@@ -109,7 +109,7 @@ INSERT INTO `order_product` (`order_id`, `order_status`, `order_total_price`, `u
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(30) COLLATE utf8_vietnamese_ci NOT NULL,
+  `product_name` varchar(50) COLLATE utf8_vietnamese_ci NOT NULL,
   `product_info` text COLLATE utf8_vietnamese_ci NOT NULL,
   `product_price` float NOT NULL,
   `product_img` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL,
@@ -117,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `category` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `product_category_fk` (`category`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `product`
@@ -129,7 +129,21 @@ INSERT INTO `product` (`id`, `product_name`, `product_info`, `product_price`, `p
 (3, 'Áo croptop in Sun Kissed', 'Áo thời trang', 210000, 'vendors/images/product/AoThunNu.jpg', 0, 4),
 (4, 'ÁO KHOÁC TÚI KHÓA KÉO AB657	', 'Áo khoác nữ thời trang', 265000, 'vendors/images/product/AoKhoac1.jpg', 200000, 6),
 (5, 'Áo khoác Jockey chống tia UV', 'Áo khoác thời trang bảo vệ cơ thể', 485000, 'vendors/images/product/AoKhoacNam.jpg', 0, 6),
-(6, 'ÁO THUN POLO NAM TS	', 'Áo thun nam cao cấp', 183000, 'vendors/images/product/AoThunNam.jpg', 150000, 5);
+(6, 'ÁO THUN POLO NAM TS	', 'Áo thun nam cao cấp', 183000, 'vendors/images/product/AoThunNam.jpg', 150000, 5),
+(7, 'Ão nam T-shirt cá»• trÃ²n in hÃ¬nh', 'Ão thun in hÃ¬nh Ä‘áº¹p', 199000, './vendors/images/product/S57TS2139001.jpg', 0, 5),
+(8, 'Ão hoodie nam', 'Ão hoodie nam cÃ¡ tÃ­nh', 549000, './vendors/images/product/H17TH20085-01.jpg', 500000, 6),
+(9, 'Quáº§n Jean nam xanh nháº¡t slimfit', 'Quáº§n Jean', 395000, './vendors/images/product/2_9de67f51e3534714919c66f0c52da729_grande.jpg', 0, 1),
+(10, 'Quáº§n Thun DÃ i Ná»‰ Phá»‘i QTD0015', 'Quáº§n dÃ i nam thá»i trang', 225000, './vendors/images/product/1a_ff6cc6bd3c59479cbce699b2edcb4cd7_grande.jpg', 20000, 1),
+(11, 'Quáº§n Kaki Nam Cáº¡p Thun Co GiÃ£n', 'Quáº§n co giÃ£n tá»‘t thoáº£i mÃ¡i khi máº·c', 250000, './vendors/images/product/1_78dfad1b88454cf49c17f24e4e14e8fd_grande.jpg', 0, 1),
+(12, 'Quáº§n Jogger Váº£i DÃ¹ QJK0024', 'Quáº§n Jogger Váº£i DÃ¹ thá»i trang', 220000, './vendors/images/product/2_0c42460283234344ab19a2e355e0898e_grande.jpg', 0, 1),
+(13, 'Quáº§n TÃ¢y Nam QTA0008', 'Quáº§n TÃ¢y Nam Cao cáº¥p', 300000, './vendors/images/product/2_a26c0f09a12242058c071d2d5d6e8f52_grande.jpg', 29000, 1),
+(14, 'QUáº¦N JEANS Ná»® á»NG LOE QJB77', 'QUáº¦N JEANS Ná»® á»NG LOE cao cáº¥p', 390000, './vendors/images/product/1-xanhnhat-qjb771010720211126127064.jpg', 359000, 2),
+(15, 'QUáº¦N TÃ‚Y CÆ  Báº¢N QB754', 'QUáº¦N TÃ¢y Ná»® cao cáº¥p', 400000, './vendors/images/product/1-den-qb754060720211513070708.jpg', 0, 2),
+(16, 'QUáº¦N SUÃ”NG á»NG Rá»˜NG QB778', 'QUáº¦N SUÃ”NG á»NG Rá»˜NG cao cáº¥p', 300000, './vendors/images/product/1-den-qb778010720211536051861.jpg', 0, 2),
+(17, 'QUáº¦N CARO á»NG Rá»˜NG QB7102', 'QUáº¦N CARO á»NG Rá»˜NG cao cáº¥p', 289000, './vendors/images/product/1-xam-qb7102060720211045449639.jpg', 269000, 2),
+(18, 'Quáº§n Short TÃ¢y BOUTON Signature', 'QUáº¦N SHORT NAM', 290000, './vendors/images/product/img_4205_cc11a65f5150427192175f2103f53259_large.jpg', 0, 3),
+(19, 'Quáº§n Short Jeans BOUTON Váº£y SÆ¡n', 'QUáº¦N SHORT NAM', 249000, './vendors/images/product/img_5165_de6dd3ea3222455090731be7303298f5_large.jpg', 0, 3),
+(20, 'Quáº§n Short Biá»ƒn BOUTON Printed', 'QUáº¦N SHORT NAM', 200000, './vendors/images/product/img_0458_af676eb016c34db48027c8eb24448445_large.jpg', 0, 3);
 
 -- --------------------------------------------------------
 
@@ -147,17 +161,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `sex` tinyint(1) NOT NULL,
   `role` tinyint(1) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `date_created`, `sex`, `role`) VALUES
-(1, 'Phuc', 'phuc@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-12-28 20:47:43', 0, 0),
+(1, 'Phuc', 'phuc@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '2021-12-28 20:47:43', 0, 0),
 (2, 'Vy', 'vy@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-12-28 20:47:43', 1, 0),
 (5, 'User1', 'user1@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-12-31 22:48:51', 0, 1),
-(6, 'User2', 'user2@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-12-31 22:48:51', 1, 1);
+(6, 'User2', 'user2@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2021-12-31 22:48:51', 1, 1),
+(8, 'aa', 'a@gmail.com', '25d55ad283aa400af464c76d713c07ad', '2022-01-04 20:46:31', 0, 1);
 
 --
 -- Các ràng buộc cho các bảng đã đổ
