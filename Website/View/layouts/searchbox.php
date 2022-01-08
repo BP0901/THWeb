05a4f3cb27adc831754ff2dd8e1542ae83e855f8
TranBaoPhuc <phuc423vn@gmail.com>
2022-01-08 23:59:59 +0700

    [Client] Fuction searching product

--- Website/View/layouts/searchbox.php
+++ Website/View/layouts/searchbox.php
@@ -1,6 +1,11 @@
 <div class="aa-search-box">
-    <form action="">
-    <input type="text" name="" id="" placeholder="Nhập Vào Đây Để Tìm Kiếm">
-    <button type="submit"><span class="fa fa-search"></span></button>
+    <form action="product.php" method="get">
+        <?php if(isset($_GET['txt_search'])){?>
+            <input type="text" name="txt_search" value="<?php echo $_GET['txt_search'];?>" id="" placeholder="Nhập Vào Đây Để Tìm Kiếm">
+        <?php } else { ?>
+            <input type="text" name="txt_search" id="" placeholder="Nhập Vào Đây Để Tìm Kiếm">
+        <?php } ?>
+    <input type="text" name="category" value="-1" hidden>
+    <button type="submit" name="search"><span class="fa fa-search"></span></button>
     </form>
 </div>
